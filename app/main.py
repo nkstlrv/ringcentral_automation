8c0b736137fea94d9feeaa87f40f7073c5b5094e
nkstlrv <stlrvnk@gmail.com>
2023-08-24 16:38:39 +0300

    exceptions handling + logging

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 import os
 from dotenv import load_dotenv
 import requests
-import json
+import logging
 from utils import format_response_json
 
 load_dotenv()
 
+logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
 
 # RingCentral API Credentials
 ACCOUNT_ID = os.getenv("ACCOUNT_ID")
@@ -23,10 +24,37 @@ def get_calls_list():
         "content-type": "application/json",
     }
 
-    response: dict = requests.get(url=calls_list_url, headers=headers).json()
+    response = requests.get(url=calls_list_url, headers=headers)
+
+    try:
+        response.raise_for_status()  # checking whether request was successful
+    except requests.exceptions.HTTPError as ex:
+        logging.error(ex)
+        return False
 
     # formatting data to return only necessary information about calls
-    data: list[dict] = format_response_json(response_data=response)
+    data: list[dict] = format_response_json(response_data=response.json())
+    return data
+
+
+def get_recording(recording_id: str):
+    base_url = f"https://platform.devtest.ringcentral.com/restapi/v1.0/account/{ACCOUNT_ID}/recording/{recording_id}"
+
+    headers = {
+        "accept": "application/json",
+        "authorization": f"Bearer {TOKEN}",
+        "content-type": "application/json",
+    }
+
+    response = requests.get(url=base_url, headers=headers)
+
+    try:
+        response.raise_for_status()  # checking whether request was successful
+    except requests.exceptions.HTTPError as ex:
+        logging.error(ex)
+        return False
+
+    data: dict = response.json()
     return data
 
 
