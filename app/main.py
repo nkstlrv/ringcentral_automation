283d2e0400b61e9e0dadecbb32b1cc67a29e07c5
nkstlrv <stlrvnk@gmail.com>
2023-08-27 11:15:44 +0300

    pretty-errors installed; webhooks initial

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 import requests
 import logging
+import pretty_errors
 from utils import format_calls_list_response_json, format_sms_list_response_json
 
 load_dotenv()
@@ -23,8 +24,8 @@ AUTH_HEADERS = {
 def get_calls_list() -> list[dict] | bool:
     calls_list_url = (
         f"https://platform.ringcentral.com/restapi/v1.0/account/"
-        f"{ACCOUNT_ID}/extension/{EXTENSION_ID}/call-log"
-    )
+        f"{ACCOUNT_ID}/extension/{EXTENSION_ID}/call-log?view=Detailed"
+    )   
 
     response = requests.get(url=calls_list_url, headers=AUTH_HEADERS)
 
@@ -41,40 +42,40 @@ def get_calls_list() -> list[dict] | bool:
     return data
 
 
-def get_recording(recording_id: str) -> list | bool:
-    recording_base_url = f"https://platform.ringcentral.com/restapi/v1.0/account/{ACCOUNT_ID}/recording/{recording_id}"
+# def get_recording(recording_id: str) -> list | bool:
+#     recording_base_url = f"https://platform.ringcentral.com/restapi/v1.0/account/{ACCOUNT_ID}/recording/{recording_id}"
 
-    response = requests.get(url=recording_base_url, headers=AUTH_HEADERS)
+#     response = requests.get(url=recording_base_url, headers=AUTH_HEADERS)
 
-    try:
-        response.raise_for_status()  # checking whether request was successful
-    except requests.exceptions.HTTPError as ex:
-        logging.error(ex)
-        return False
+#     try:
+#         response.raise_for_status()  # checking whether request was successful
+#     except requests.exceptions.HTTPError as ex:
+#         logging.error(ex)
+#         return False
 
-    data: dict = response.json()
-    return data
+#     data: dict = response.json()
+#     return data
 
 
-def get_sms_list() -> list[dict] | bool:
-    sms_base_url = (
-        f"https://platform.ringcentral.com/restapi/v1.0/"
-        f"account/{ACCOUNT_ID}/extension/{EXTENSION_ID}/message-store"
-    )
+# def get_sms_list() -> list[dict] | bool:
+#     sms_base_url = (
+#         f"https://platform.ringcentral.com/restapi/v1.0/"
+#         f"account/{ACCOUNT_ID}/extension/{EXTENSION_ID}/message-store"
+#     )
 
-    response = requests.get(url=sms_base_url, headers=AUTH_HEADERS)
+#     response = requests.get(url=sms_base_url, headers=AUTH_HEADERS)
 
-    try:
-        response.raise_for_status()  # checking whether request was successful
-    except requests.exceptions.HTTPError as ex:
-        logging.error(ex)
-        return False
+#     try:
+#         response.raise_for_status()  # checking whether request was successful
+#     except requests.exceptions.HTTPError as ex:
+#         logging.error(ex)
+#         return False
 
-    data: list[dict] = format_sms_list_response_json(response_sms_data=response.json())
-    return data
+#     data: list[dict] = format_sms_list_response_json(response_sms_data=response.json())
+#     return data
 
 
 if __name__ == "__main__":
     print(get_calls_list())
-    print(get_sms_list())
-    print(get_recording("2167092738043"))
+    # print(get_sms_list())
+    # print(get_recording("2167092738043"))
